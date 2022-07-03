980eefa2c1ec69894f62611b15f10b37c957f5b1
JoseMM2002 <jeolmmt@gmail.com>
2022-07-03 11:50:49 -0500

    issues

--- proyect/proyect/urls.py
+++ proyect/proyect/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('app.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC__ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
